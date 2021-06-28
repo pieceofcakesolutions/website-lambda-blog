9c7d3b56af837c4fdab90d90d6777724b1b64515
morrell <jemorrell@gmail.com>
2021-06-27 20:42:36 -0500

    Initial commit

---
+++ src/cake-lambda-blog/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace cake_lambda_blog
+{
+    public class Blog
+    {
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+        public DateTime ModifiedTimestamp { get; set; }
+    }
+}
